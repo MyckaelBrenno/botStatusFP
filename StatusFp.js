1ab114d1ae6628e87fc29d8338bf5396297fdaa9
Myckael Brenno <myckael.brenno@pactomais.com.br>
2022-01-05 14:36:52 -0300

    Status enviado automaticamente no ready | setInterval para verificacao constante do status do servico

--- StatusFp.js
+++ StatusFp.js
@@ -1,55 +1,78 @@
 const config = require("./config.json");
 
 const { Client, Intents } = require('discord.js');
+const { default: axios, Axios } = require("axios");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 client.on("ready", () => {
-    console.log(`Bot foi iniciado com sucesso!`)
+    console.log(`Bot foi iniciado com sucesso!`);
+
+    const axios = require('axios').default;
+
+    const canal = client.channels.cache.get('927573905458212955');
+
+    isMsgErrorEnviado = false;
+
+ setInterval(() => {
+    axios({
+        method: 'get',
+        url: 'http://localhost:8080/cliente/1',
+        //timeout: 2000
+
+    }).then(function (response) {
+        
+        if (isMsgErrorEnviado) {
+            console.log("Status: " + response.status);
+            console.log(response.statusText);
+            canal.send({ content: response.statusText + ", o serviço do MS está de volta!" });
+            isMsgErrorEnviado = false;
+        }
+        // message.channel.send(response.statusText + ", Status 200!");
+
+    }).catch(function (error) {
+
+        if (!isMsgErrorEnviado) {
+            isMsgErrorEnviado = true;
+            console.log("Status: " + error.status);
+            console.log("StatusText: " + error.statusText);
+            canal.send({ content: error.statusText + ", O Portal Farmácia do MS está fora do ar!" });
+        }
+        //message.reply(error.statusText + ", Error!");
+    })
+
+}, 10000);
+
 })
 
-// client.on("messageCreate", function (message) {
-//     if (message.author.bot) return;
+client.login(config.token);
 
-//     //Ping
-//     var ping = require('ping');
-//     var hosts = ['100.126.2.206', '100.127.6.150', '100.126.2.218'];
-//     hosts.forEach(function (host) {
-//         ping.sys.probe(host, function (isAlive) {
-//             var msg = isAlive ? 'host ' + host + ' está ativo' : 'host ' + host + ' está morto';
-//             console.log(msg);
-
-//             if (message.content === "ping"){
-//                 message.reply(msg);
-//             }
-//         });
-//     });
-// });
-
-const axios = require('axios').default;
-
+/*
 client.on("messageCreate", function (message) {
     if (message.author.bot) return;
 
-    axios({
-        method: 'get',
-        url: 'https://farmaciapopular-portal.saude.gov.br/farmaciapopular-portal/login.jsf',
-    }).then(function (response) {
-        console.log(response.status);
-        console.log(response.statusText);
-
-        message.reply(response.statusText + ", Status 200" );
-        //message.reply(response.status);
-    }).catch(function (error) {
-        console.log(error.status);
-        console.log(error.statusText);
-        console.log(error.headers);
-        message.reply(error.statusText + ", Error!");
-    })
-
 });
+*/
 
-client.login(config.token);
+/*
+ client.on("messageCreate", function (message) {
+     if (message.author.bot) return;
+
+     //Ping
+     var ping = require('ping');
+     var hosts = ['100.126.2.206', '100.127.6.150', '100.126.2.218'];
+     hosts.forEach(function (host) {
+         ping.sys.probe(host, function (isAlive) {
+             var msg = isAlive ? 'host ' + host + ' está ativo' : 'host ' + host + ' está morto';
+             console.log(msg);
+
+             if (message.content === "ping"){
+                 message.reply(msg);
+             }
+         });
+     });
+ });
+*/
 
 //**Comando para criar as REQUIREMENTS das dependências do projeto */
 //pip freeze > requirements.txt
